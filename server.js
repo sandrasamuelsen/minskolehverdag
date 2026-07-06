0823aae6f644ebf97a76083856c6c40031c9e710
sandrasamuelsen <sandras.samuelsen@gmail.com>
2026-07-06 14:50:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
-import sqlite3 from "sqlite3";
 
 const app = express();
-const db = new sqlite3.Database("./db.sqlite");
 
 app.use(cors());
 app.use(express.json());
@@ -11,50 +9,17 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/data", express.static("data"));
 
-db.run(`
-CREATE TABLE IF NOT EXISTS responses (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  participantId TEXT,
-  mode TEXT,
-  answers TEXT
-)
-`);
-
 app.post("/submit", (req, res) => {
 
-  const { participantId, mode, answers } = req.body;
+    console.log(req.body);
 
-  db.run(
-    "INSERT INTO responses (participantId, mode, answers) VALUES (?,?,?)",
-    [
-      participantId,
-      mode,
-      JSON.stringify(answers)
-    ]
-  );
-
-  res.json({ ok: true });
-
-});
-
-app.get("/responses", (req, res) => {
-
-  db.all(
-    "SELECT * FROM responses",
-    (err, rows) => {
-
-      if (err) {
-        return res.status(500).json(err);
-      }
-
-      res.json(rows);
-    }
-  );
-
+    res.json({
+        ok: true
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server kjører");
+    console.log("Server kjører");
 });
